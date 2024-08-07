6c5da026d5cee52c7196474ac746e66d19fdd47b
kaeltheron <105551131+kaeltheron@users.noreply.github.com>
2024-08-07 14:09:00 +0800

    Create dynamic-providers.js

---
+++ dynamic-providers.js
@@ -0,0 +1,19 @@
+
+
+// 获取前一天的日期
+const date = new Date();
+date.setDate(date.getDate() - 1);
+
+const year = date.getFullYear();
+const month = (`0${date.getMonth() + 1}`).slice(-2); // 补零
+const day = (`0${date.getDate()}`).slice(-2); // 补零
+const fullDate = `${year}${month}${day}`;
+
+const overrideContent = `
+proxy-providers:
+  dynamic-provider:
+    url: "https://nodefree.githubrowcontent.com/${year}/${month}/${fullDate}.yaml"
+`;
+$done({
+  overrideContent
+})
